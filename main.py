54f5aa349e95ada2badf27dc504555b629c4dbd5
Oliver Thim <oliverthim@olivers-macbook-air.local>
2017-12-05 13:20:03 +0100

    Laggt in en stomme för programmet med lite kommentarer. Har kopierat lite metoder från Christians program.

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+# This program is based on a program by Christian Forssen.
+
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
 import radialSolver.radialSolver as rs
 
+def heaviside(x):
+    return 0.5*(np.sign(x) + 1)
 
-print (rs.test(-3))
-print("Brigers bubblande boogie biter bäst")
+def pot_step(r,V0=1,c=1):
+    return -V0 * heaviside(c-r)
 
-for x in range(0,3):
-    print("Test loop nr",x)
+def pot_coulomb(r,Z=1):
+    return -Z/r
 
-print("hej")
+def pot_gauss(r,V0=1,c=1):
+    return -V0*np.exp(-r**2/(2*c**2))
+
+R = np.logspace(-7,2.5,5000) # Logarithmic mesh for our integration: [10^-7,...10^2.5]
+Z=1                      # Charge of the nucleous
+nmax = 5                 # Maximum principal state
+lmax = 0                 # Maximum angular momentum
+
+def pot(r):
+    #return pot_coulomb(r,Z)+pot_gauss(r)
+    return pot_coulomb(r,Z)+pot_step(r)
+
+# Computes eigenstates requested by lmax and nmax
+bound_states = bs.FindBoundStates(R,lmax,nmax,pot)
--- radialSolver/radialSolver.py
+++ radialSolver/radialSolver.py
@@ -1,6 +1,45 @@
-import numpy as np
-import scipy
-import matplotlib
+# This program is based on a program by Christian Forssen.
 
-def test(x):
-    return np.abs(x)
+import numpy as np
+import scipy as sp
+import matplotlib as plt
+
+
+def _NumerovSolve(E,R,l,pot):
+    # Solve the radial schöringer equation using Numerovs method.
+
+def _SolveSchroedinger(E,R,l,pot):
+    """ Integrates Schroedinger equation given a energy E, and angular momentum
+    eigenvalue l, and potential pot(r).
+    For stability, we integrate Schroedinger equation starting at infinity
+    and integrating down to zero.
+    We use initial condition at infinity : u(infinity)=0, and u'(infinity)=small.
+    In order that solution is of the order of unity, we normalize it to (roughly) the peak
+    value of the function.
+    """
+
+
+def _Shoot(E, R, l, pot):
+    """ Given a energy E (momentum eigenvalue l, and potential pot(r)), and initial condition
+        at infinity (u(infinity)=0, u'(infinity)=small) it finds the value of u(r=0).
+        For the state to be bound state, we have the following condition: u(r=0)==0.
+    """
+
+
+def FindBoundStates(R,lmax,nmax,pot,MaxSteps=1000,Etol=1e-17):
+    """ For each momentum eigenvalue l=[0,...lmax], it find first nmax-l bound-states.
+    It first brackets roots of function Shoot using a logarithmic mesh dense at zero.
+    It later refinds the roots by optimize.brentq method.
+    """
+
+    # For each momentum eigenvalue l
+
+        # Find nmax bound states
+
+        # Find a higher and lower boundry for E.
+
+        # Find E so u(r=0) = 0.
+
+        # Print result
+
+    # Return the bound states
