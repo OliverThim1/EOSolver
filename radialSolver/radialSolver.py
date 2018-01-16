dad9e63bbb21770ec5b8c9cf66580267bf2609d1
Oliver Thim <oliverthim@olivers-macbook-air.local>
2018-01-16 17:08:54 +0100

    Skrivit klart _NumerovSolve (typ) och även _Shoot (i stora drag) / E & O

--- radialSolver/radialSolver.py
+++ radialSolver/radialSolver.py
@@ -8,21 +8,30 @@ import matplotlib as plt
 def _NumerovSolve(g,s,R,h,start1=0,start2=1e-4):
     """ Solves differential equation of the form y'' = -g(x)y + s(x)
         for every point in R using numerovs method.
+        <Start2> only scales the solution.
+
+        TODO: Improve array system to numpy.array.
     """
-    # Solves backwards so ew flip R.
+    # Solves backwards so flip R.
     R0 = R[::-1]
 
-    y=[start1,start2]
+    y=[start1,start2] # Function value array start from the rear.
 
     for i in range(1,len(R)-1):
         xn = R[i]
         xnm = R[i-1]
         xnp = R[i+1]
 
+        # Numerovs method.
         x = (  2.*y[i]*(1-((5/12)*(h**2))*g(xn)) - y[i-1]*(1+((h**2)/12)*g(xnm)) + ((h**2)/12)*(s(xnp) + 10*s(xn) + s(xnm))  ) / (1 + ((h**2)/12)*g(xnp))
         y.append(x)
 
-    return y[::-1]
+    # Take the norm of the y-vector; divide with it's maximum.
+    norm = max(y[:int(len(R) * 0.9)])
+
+    y = y[::-1] # Flips back.
+    u = [x / norm for x in y] # Normalizes.
+    return u
 
 def _SolveSchroedinger(E,R,l,pot):
     """ Integrates Schroedinger equation given a energy E, and angular momentum
@@ -32,7 +41,11 @@ def _SolveSchroedinger(E,R,l,pot):
     We use initial condition at infinity : u(infinity)=0, and u'(infinity)=small.
     In order that solution is of the order of unity, we normalize it to (roughly) the peak
     value of the function.
+
+    TODO: Find functions g(x), s(x), so that we can solve the diff.equation with _NumerovSolve.
     """
+
+
 
 
 def _Shoot(E, R, l, pot):
@@ -41,6 +54,11 @@ def _Shoot(E, R, l, pot):
         For the state to be bound state, we have the following condition: u(r=0)==0.
     """
 
+    u = _SolveSchroedinger(E, R, l, pot)
+
+    # Vi vill ha värdet i r=0, därför interpolerar vi sista biten i.o.m att
+    # R[0] inte riktigt.
+    return u[0] + (u[1] - u[0]) * (0.0 - R[0]) / (R[1] - R[0])
 
 def FindBoundStates(R,lmax,nmax,pot,MaxSteps=1000,Etol=1e-17):
     """ For each momentum eigenvalue l=[0,...lmax], it find first nmax-l bound-states.
