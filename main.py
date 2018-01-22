6962f127c820cfbf6df46d224f6ba8829c846f4e
Oliver Thim <oliverthim@olivers-macbook-air.local>
2018-01-22 17:07:49 +0100

    Programmet funkar med en linjärt fördelad R-vektor.

--- main.py
+++ main.py
@@ -17,9 +17,10 @@ def pot_coulomb(r,Z=1):
 def pot_gauss(r,V0=1,c=1):
     return -V0*np.exp(-r**2/(2*c**2))
 
-R = np.logspace(-7,2.5,5000) # Logarithmic mesh for our integration: [10^-7,...10^2.5]
+#R = np.logspace(-4,2.5,5000) # Logarithmic mesh for our integration: [10^-7,...10^2.5]
+R = np.linspace(1e-7, 250,10000)
 Z=1                      # Charge of the nucleous
-nmax = 5                 # Maximum principal state
+nmax = 5             # Maximum principal state
 lmax = 0                 # Maximum angular momentum
 
 def pot(r):
@@ -29,10 +30,8 @@ def pot(r):
 # Computes eigenstates requested by lmax and nmax
 #bound_states = bs.FindBoundStates(R,lmax,nmax,pot)
 
-R = np.linspace(0,10,1000)
 
-
-''' Test av Numerov Solve.
+"""# Test av Numerov Solve.
 def g(x):
     return 1
 def s(x):
@@ -42,7 +41,7 @@ y = rs._NumerovSolve(g,s,np.linspace(0,10,1000),0.01)
 
 plt.plot(R, y)
 plt.show()
-'''
+"""
 
 bound_states = rs.FindBoundStates(R,lmax,nmax,pot)
 
--- radialSolver/radialSolver.py
+++ radialSolver/radialSolver.py
@@ -3,11 +3,12 @@
 import numpy as np
 import pylab as p
 from scipy import *
-from scipy import optimize
-import matplotlib as plt
+from scipy import\
+    optimize
+import matplotlib.pyplot as plt
 
 
-def _NumerovSolve(g,s,R,h,start1=0,start2=1e-4):
+def _NumerovSolve(g,s,R,start1=0,start2=1e-4):
     """ Solves differential equation of the form y'' = -g(x)y + s(x)
         for every point in R using numerovs method.
         <Start2> only scales the solution.
@@ -16,13 +17,14 @@ def _NumerovSolve(g,s,R,h,start1=0,start2=1e-4):
     """
     # Solves backwards so flip R.
     R0 = R[::-1]
-
     y=[start1,start2] # Function value array start from the rear.
 
     for i in range(1,len(R)-1):
-        xn = R[i]
-        xnm = R[i-1]
-        xnp = R[i+1]
+        xn = R0[i]
+        xnm = R0[i-1]
+        xnp = R0[i+1]
+
+        h = np.abs(xnp-xn)
 
         # Numerovs method.
         x = (  2.*y[i]*(1-((5/12)*(h**2))*g(xn)) - y[i-1]*(1+((h**2)/12)*g(xnm)) + ((h**2)/12)*(s(xnp) + 10*s(xn) + s(xnm))  ) / (1 + ((h**2)/12)*g(xnp))
@@ -32,11 +34,9 @@ def _NumerovSolve(g,s,R,h,start1=0,start2=1e-4):
     norm = max(y[:int(len(R) * 0.9)])
 
     y = y[::-1] # Flips back.
+
     u = [x / norm for x in y] # Normalizes.
     return u
-
-
-
 
 def _SolveSchroedinger(E,R,l,pot):
     """ Integrates Schroedinger equation given a energy E, and angular momentum
@@ -57,7 +57,6 @@ def _SolveSchroedinger(E,R,l,pot):
     u'' = (2*(V-E) + l(l+1)/r^2)*u
     _NumerovSolve wants a differential equation of the form y'' = -g(x)y + s(x), so we construct g(x), s(x).
     """
-    h = 0.01 # Steglängd
 
     def g(r):
         return (2 * (E - pot(r)) - l*(l + 1) / r ** 2)
@@ -65,7 +64,7 @@ def _SolveSchroedinger(E,R,l,pot):
     def s(r):
         return 0
 
-    return _NumerovSolve(g, s, R, h)
+    return _NumerovSolve(g, s, R)
 
 
 def _Shoot(E, R, l, pot):
@@ -78,7 +77,9 @@ def _Shoot(E, R, l, pot):
 
     # Vi vill ha värdet i r=0, därför interpolerar vi sista biten i.o.m att
     # R[0] inte riktigt.
-    return u[0] + (u[1] - u[0]) * (0.0 - R[0]) / (R[1] - R[0])
+    x = u[0] + (u[1] - u[0]) * (0.0 - R[0]) / (R[1] - R[0])
+
+    return x
 
 def FindBoundStates(R,lmax,nmax,pot,MaxSteps=1000,Etol=1e-17):
     """ For each momentum eigenvalue l=[0,...lmax], it find first nmax-l bound-states.
@@ -106,6 +107,9 @@ def FindBoundStates(R,lmax,nmax,pot,MaxSteps=1000,Etol=1e-17):
             print ('Bound state # %2i (l=%2i): E=%12.9f (Pure Coulomb: E_c=%12.9f; ratio=%6.4f)' \
             % (nfound, l, Eb0, E_coul, Eb0 / E_coul))
             nfound+=1
+            #U = _SolveSchroedinger(Eb0, R, l, pot)
+            #plt.semilogx(R, U)
+            #plt.show()
     return Eb
 
 
